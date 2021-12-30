dd2f97833c31ee070ef5d8027662d8e02944d6ef
Long Nguyen <anonyhost@gmail.com>
2021-12-30 16:29:55 +0700

    Cudpate kfolds strategy

--- non_deep_method/xgb_model/model_builder.py
+++ non_deep_method/xgb_model/model_builder.py
@@ -1,44 +1,82 @@
 from lightgbm import LGBMClassifier, early_stopping, log_evaluation
 
 from non_deep_method.data_builder.xy_data_builder import data_builder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.metrics import roc_auc_score
+import numpy as np
 
 
 class ModelBuilder:
-    def __init__(self, top_n):
-        self.clf = LGBMClassifier(n_jobs=4,
-                                  n_estimators=4000,
-                                  learning_rate=0.02,
-                                  num_leaves=34,
-                                  colsample_bytree=0.9497036,
-                                  subsample=0.8715623,
-                                  max_depth=8,
-                                  reg_alpha=0.041545473,
-                                  reg_lambda=0.0735294,
-                                  min_split_gain=0.0222415,
-                                  min_child_weight=39.3259775,
-                                  verbose=-1
-                                  )
-        self.X_train, self.y_train = data_builder.build_data_with_features(top_n=top_n, phase='train_phase',
-                                                                           prefix='train_ques')
-        self.X_test, self.y_test = data_builder.build_data_with_features(top_n=top_n, phase='test_phase',
-                                                                         prefix='train_ques')
+    def __init__(self):
+        self.clf = []
 
-    def test(self):
-        pred_prob = self.clf.predict_proba(X=self.X_test)
-        pred_prob = pred_prob[:, 1]
-        print(roc_auc_score(y_true=self.y_test, y_score=pred_prob))
+    def test(self, X, y):
+        # [n_clf * n_sample]
+        pred_prob = np.array([single_clf.predict_proba(X=X)[:, 1] for single_clf in self.clf])
 
-    def start_train(self):
-        x_train, x_val, y_train, y_val = train_test_split(self.X_train, self.y_train, test_size=0.2)
-        self.clf.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_val, y_val)],
-                     callbacks=[early_stopping(stopping_rounds=200), log_evaluation(period=200)],
-                     eval_metric='auc')
+        pred_prob = np.mean(pred_prob, axis=0)
 
+        print('roc-auc score one test set: ', roc_auc_score(y_true=y, y_score=pred_prob))
 
-model_builder = ModelBuilder(top_n=50)
-model_builder.start_train()
+    def train_single_model(self, X, y):
+        self.clf = []
+        print('Start training single xgb model')
+        x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+        single_clf = LGBMClassifier(n_jobs=4,
+                                    n_estimators=4000,
+                                    learning_rate=0.02,
+                                    num_leaves=34,
+                                    colsample_bytree=0.9497036,
+                                    subsample=0.8715623,
+                                    max_depth=8,
+                                    reg_alpha=0.041545473,
+                                    reg_lambda=0.0735294,
+                                    min_split_gain=0.0222415,
+                                    min_child_weight=39.3259775,
+                                    verbose=-1
+                                    )
+        single_clf.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_val, y_val)],
+                       callbacks=[early_stopping(stopping_rounds=200), log_evaluation(period=200)],
+                       eval_metric='auc')
+        self.clf.append(single_clf)
+
+    def train_k_fold(self, X, y):
+        skf = StratifiedKFold(n_splits=6)
+        self.clf = []
+        for fold_id, (train_index, val_index) in enumerate(skf.split(X=X, y=y)):
+            print('Start training fold: ', fold_id)
+            single_clf = LGBMClassifier(n_jobs=4,
+                                        n_estimators=4000,
+                                        learning_rate=0.02,
+                                        num_leaves=34,
+                                        colsample_bytree=0.9497036,
+                                        subsample=0.8715623,
+                                        max_depth=8,
+                                        reg_alpha=0.041545473,
+                                        reg_lambda=0.0735294,
+                                        min_split_gain=0.0222415,
+                                        min_child_weight=39.3259775,
+                                        verbose=-1
+                                        )
+            x_train = X[train_index]
+            x_val = X[val_index]
+            y_train = y[train_index]
+            y_val = y[val_index]
+            single_clf.fit(x_train, y_train, eval_set=[(x_train, y_train), (x_val, y_val)],
+                           callbacks=[early_stopping(stopping_rounds=200), log_evaluation(period=400)],
+                           eval_metric='auc')
+            self.clf.append(single_clf)
+
 
 if __name__ == '__main__':
-    model_builder.test()
+    top_n = 50
+    train_x, train_y = data_builder.build_data_with_features(top_n=top_n, phase='train_phase',
+                                                             prefix='train_ques')
+    test_x, test_y = data_builder.build_data_with_features(top_n=top_n, phase='test_phase',
+                                                           prefix='train_ques')
+    model_builder = ModelBuilder()
+
+    # model_builder.train_single_model(X=train_x, y=train_y)
+    model_builder.train_k_fold(X=train_x, y=train_y)
+
+    model_builder.test(X=test_x, y=test_y)
