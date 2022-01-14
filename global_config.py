fdd5686f5ef6fdd62fdb14b1a649badda58347cd
Nguyễn Hải Long <34580125+anonyhostvn@users.noreply.github.com>
2022-01-14 15:34:23 +0700

    change path

--- global_config.py
+++ global_config.py
@@ -1,9 +1,11 @@
-LEGAL_CORPUS_PATH = 'zac2021-ltr-data/legal_corpus.json'
-DATA_QUESTION_PATH = 'zac2021-ltr-data/train_question_answer.json'
-TEST_QUESTION_PATH = 'zac2021-ltr-data/public_test_question.json'
-SEGMENTED_LEGAL_CORPUS = 'segemented_data/segmented_corpus.json'
-SEGMENTED_DATA_QUESTION = 'segemented_data/train_ques_segmented.json'
-SEGMENTED_TEST_QUESTION = 'segemented_data/test_ques_segmented.json'
-SEGMENTED_TITLE_PATH = 'segemented_data\segmented_title.json'
-TRAIN_IDX = 'data_spliter/train_idx.json'
-TEST_IDX = 'data_spliter/test_idx.json'
+import os
+
+LEGAL_CORPUS_PATH = os.path.join('zac2021-ltr-data', 'legal_corpus.json')
+DATA_QUESTION_PATH = os.path.join('zac2021-ltr-data', 'train_question_answer.json')
+TEST_QUESTION_PATH = os.path.join('zac2021-ltr-data', 'public_test_question.json')
+SEGMENTED_LEGAL_CORPUS = os.path.join('segemented_data', 'segmented_corpus.json')
+SEGMENTED_DATA_QUESTION = os.path.join('segemented_data', 'train_ques_segmented.json')
+SEGMENTED_TEST_QUESTION = os.path.join('segemented_data', 'test_ques_segmented.json')
+SEGMENTED_TITLE_PATH = os.path.join('segemented_data', 'segmented_title.json')
+TRAIN_IDX = os.path.join('data_spliter', 'train_idx.json')
+TEST_IDX = os.path.join('data_spliter', 'test_idx.json')
