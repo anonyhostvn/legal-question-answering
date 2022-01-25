24f6ee01c770a010d429c485bf5c4aa798426425
Long Nguyen <anonyhost@gmail.com>
2022-01-25 14:32:48 +0700

    add find aidx in legal_corpus

--- non_deep_method/config.py
+++ non_deep_method/config.py
@@ -1,4 +1,3 @@
-LOAD_SAMPLE_SIZE = 100
 FT_EMBED_DIM = 300
 ESP = 1e-10
 CACHE_DIR = '/Users/LongNH/Workspace/ZaloAIChallenge/non_deep_method/cached'
--- non_deep_method/corpus_builder/legal_corpus.py
+++ non_deep_method/corpus_builder/legal_corpus.py
@@ -1,21 +1,29 @@
 import json
 
 from global_config import LEGAL_CORPUS_PATH, SEGMENTED_LEGAL_CORPUS
-from non_deep_method.config import LOAD_SAMPLE_SIZE
 from non_deep_method.corpus_builder.abstract_corpus import AbstractCorpus
 
 
 class LegalCorpus(AbstractCorpus):
-    def __init__(self, corpus_json_path, corpus_segmented_path, sample_size=None):
+    def __init__(self, corpus_json_path, corpus_segmented_path):
         with open(corpus_json_path, 'r') as f:
-            self.json_corpus = json.load(f)[:sample_size]
+            self.json_corpus = json.load(f)
+            self.lis_article = [
+                {**article_obj, 'law_id': law_obj.get('law_id')} for law_obj in self.json_corpus for article_obj in
+                law_obj.get('articles')
+            ]
 
         with open(corpus_segmented_path, 'r') as f:
-            self.segmented_corpus = json.load(f)[:sample_size]
+            self.segmented_corpus = json.load(f)
 
         super().__init__(self.json_corpus, self.segmented_corpus)
+
+    def get_aidx(self, law_id, article_id):
+        for aidx, article_obj in enumerate(self.lis_article):
+            if article_obj.get('law_id') == law_id and article_obj.get('article_id') == article_id:
+                return aidx
 
 
 if __name__ == '__main__':
     legal_corpus = LegalCorpus(corpus_json_path=LEGAL_CORPUS_PATH,
-                               corpus_segmented_path=SEGMENTED_LEGAL_CORPUS, sample_size=LOAD_SAMPLE_SIZE)
+                               corpus_segmented_path=SEGMENTED_LEGAL_CORPUS)
