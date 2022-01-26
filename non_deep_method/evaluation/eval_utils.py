c1ca8bb2b213fdb5ea31f28148bd24b29a6ad6b4
Long Nguyen <anonyhost@gmail.com>
2022-01-26 13:54:40 +0700

    update model evaluation

--- non_deep_method/evaluation/eval_utils.py
+++ non_deep_method/evaluation/eval_utils.py
@@ -8,6 +8,13 @@ def calculate_f2i(lis_ground_truth, lis_predict):
     return (5 * precision * recall) / (4 * precision + recall + ESP)
 
 
+def calculate_f2i_spec(lis_ground_truth, lis_predict):
+    true_predict = len([i for i in lis_predict if i in lis_ground_truth])
+    precision = true_predict / (ESP + len(lis_predict))
+    recall = true_predict / (ESP + len(lis_ground_truth))
+    return (5 * precision * recall) / (4 * precision + recall + ESP), precision, recall
+
+
 def calculate_recall(lis_ground_truth, lis_predict):
     true_predict = len([i for i in lis_predict if i in lis_ground_truth])
     return true_predict / (ESP + len(lis_ground_truth))
